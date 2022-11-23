cddeabad12d6296a492641ddb182005937c44745
Daan van de Westelaken <daan84@mac.com>
2022-11-23 12:42:16 +0100

    Initial Commit

---
+++ ScreenshotExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScreenshotExample
+//
+//  Created by Daan van de Westelaken on 23/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
